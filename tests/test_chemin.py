2dd22abf1ab6bc0c317bdbf750dadaf97bc92dc0
Dimitri <bond2102@usherbrooke.ca>
2022-09-22 13:29:36 -0400

    Modification de la génération des G

--- tests/test_chemin.py
+++ tests/test_chemin.py
@@ -3,6 +3,7 @@
 """
 Test du chemin pris dans la zone de Brillouin.
 """
+import numpy as np
 import matplotlib.pyplot as plt
 from graphene_ge2d.struc_bande.write_matrix import tracer_chemin
 
@@ -12,5 +13,18 @@ def test_chemin():
 
     fig, ax = plt.subplots()
     ax.plot(path[:, 0], path[:, 1], "o", markersize=2)
+    ratio = path[1:, 1] / path[1:, 0]
+    section1 = np.append(np.array([True]), np.isclose(ratio, ratio[0]))
+    section3 = np.append(np.array([False]), ratio == 0.)
+    section2 = np.logical_not(np.logical_or(section1, section3))
+    point_m = path[section1, 0][-1], path[section1, 1][-1]
+    point_k = path[section2, 0][-1], path[section2, 1][-1]
+    point_gamma = path[:, 0][-1], path[:, 1][-1]
+    angle_gamma = np.arctan(ratio[0])
+
+    plt.annotate("Point M", point_m)
+    plt.annotate("Point K", point_k)
+    plt.annotate(r"Point $\Gamma$", point_gamma, (point_gamma[0], point_gamma[1] + 0.03))
+    plt.annotate(r"$\alpha=" + str(np.round(angle_gamma * 360 / (2 * np.pi), 4)) + "$", point_gamma)
     fig.savefig("Images/chemin.png")
 
--- tests/test_construction_matrice.py
+++ tests/test_construction_matrice.py
@@ -9,7 +9,7 @@ import numpy as np
 from scipy.special import j1
 import pytest
 import graphene_ge2d.struc_bande.matrice_coef as matrix
-from graphene_ge2d.struc_bande._constantes import r_0_bar, eta, eps, v_0, z_bar, e_0_inv, nc
+from graphene_ge2d.struc_bande._constantes import r_0_bar, eta, eps, v_0, z_bar, nc
 
 
 def test_matrice_explicite():
@@ -26,13 +26,13 @@ def test_matrice_explicite():
     g_1 = np.array([0, 0])
     g_2 = b_2
     g_3 = - b_1
-    g_4 = - b_2 - b_1
+    #g_4 = - b_2 - b_1
     g_5 = - b_2
     g_6 = b_1
-    g_7 = b_2 + b_1
+    #g_7 = b_2 + b_1
     g_8 = - b_1 + b_1
     g_9 = b_2 - b_1
-    g_array = np.array([g_1, g_2, g_3, g_4, g_5, g_6, g_7, g_8, g_9])
+    g_array = np.array([g_1, g_2, g_3, g_5, g_6, g_8, g_9])
 
     # Fonction des coefficients de fourier
 
@@ -45,57 +45,49 @@ def test_matrice_explicite():
         somme = 1 + np.exp(- 1j * 2 * np.pi * np.dot(g, c2) / eta)
         return v_0 * r_0_bar * bessel_boi * exponentielle * somme / (eps * eta)
 
-    diag_principale = e_0_inv * np.array([
+    diag_principale = np.array([
             g_1[0]*g_1[0] + g_1[1]*g_1[1],
             g_2[0]*g_2[0] + g_2[1]*g_2[1],
             g_3[0]*g_3[0] + g_3[1]*g_3[1],
-            g_4[0]*g_4[0] + g_4[1]*g_4[1],
             g_5[0]*g_5[0] + g_5[1]*g_5[1],
             g_6[0]*g_6[0] + g_6[1]*g_6[1],
-            g_7[0]*g_7[0] + g_7[1]*g_7[1],
             g_8[0]*g_8[0] + g_8[1]*g_8[1],
             g_9[0]*g_9[0] + g_9[1]*g_9[1],
     ]) / nc
-    test_diag = e_0_inv * np.array([
+    test_diag = np.array([
             np.dot(g_1, g_1),
             np.dot(g_2, g_2),
             np.dot(g_3, g_3),
-            np.dot(g_4, g_4),
             np.dot(g_5, g_5),
             np.dot(g_6, g_6),
-            np.dot(g_7, g_7),
             np.dot(g_8, g_8),
             np.dot(g_9, g_9),
     ]) / nc
-    ##print(f"{diag_principale=}")
+    print(f"{diag_principale=}")
     #print(f"{test_diag=}")
     assert np.isclose(test_diag, diag_principale).all()
-    diag_sup = e_0_inv * np.array([
+    diag_sup = np.array([
             - coefficients_u(g_1 - g_2),
             - coefficients_u(g_2 - g_3),
-            - coefficients_u(g_3 - g_4),
-            - coefficients_u(g_4 - g_5),
+            - coefficients_u(g_3 - g_5),
             - coefficients_u(g_5 - g_6),
-            - coefficients_u(g_6 - g_7),
-            - coefficients_u(g_7 - g_8),
+            - coefficients_u(g_6 - g_8),
             - coefficients_u(g_8 - g_9),
     ])
-    diag_inf = e_0_inv * np.array([
+    diag_inf = np.array([
             - coefficients_u(- g_1 + g_2),
             - coefficients_u(- g_2 + g_3),
-            - coefficients_u(- g_3 + g_4),
-            - coefficients_u(- g_4 + g_5),
+            - coefficients_u(- g_3 + g_5),
             - coefficients_u(- g_5 + g_6),
-            - coefficients_u(- g_6 + g_7),
-            - coefficients_u(- g_7 + g_8),
+            - coefficients_u(- g_6 + g_8),
             - coefficients_u(- g_8 + g_9),
     ])
     #print(f"{g_array=}")
-    diags = matrix.get_matrice(9, np.array([0, 0]), 3, g_array)
+    diags = matrix.get_matrice(7, np.array([0, 0]), 3, g_array)
     #print(f"{diag_principale=}")
     print(f"{diag_sup=}")
     print(f"{diag_inf=}")
-    #print(f"{diags[:, 0]=}")
+    print(f"{diags[:, 0]=}")
     print(f"{diags[:, 1]=}")
     print(f"{diags[:, 2]=}")
     assert np.isclose(diag_principale, diags[:, 0]).all()
