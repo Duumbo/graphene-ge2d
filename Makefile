5f4c1486fc3b764aec38e0b86fe4273070e5c41a
Dimitri <bond2102@usherbrooke.ca>
2022-10-04 10:05:09 -0400

    New results

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ all: Images/structure_de_bande.png Images/map_potentiel.png
 Images/structure_de_bande.png: raw_data/chemin_norm.npy raw_data/list_eigvals.npy src/graphene_ge2d/struc_bande/generate_graph.py
 	python src/graphene_ge2d/ -g &
 
-raw_data/diagonals.npy: src/graphene_ge2d/struc_bande/_constantes.py src/graphene_ge2d/struc_bande/write_matrix.py src/graphene_ge2d/struc_bande/matrice_coef.py
+raw_data/diagonals.npy: src/graphene_ge2d/struc_bande/_constantes.py src/graphene_ge2d/struc_bande/write_matrix.py src/graphene_ge2d/struc_bande/matrice_coef.py src/graphene_ge2d/struc_bande/coquilles.py
 	python src/graphene_ge2d/ -m
 
 raw_data/list_eigvals.npy: raw_data/diagonals.npy raw_data/chemin.npy src/graphene_ge2d/struc_bande/compute_eigenvalues.py
--- tests/test_construction_matrice.py
+++ tests/test_construction_matrice.py
@@ -8,8 +8,14 @@ Tests unitaire comportant:
 import numpy as np
 from scipy.special import j1
 import pytest
+import pandas as pd
 import graphene_ge2d.struc_bande.matrice_coef as matrix
-from graphene_ge2d.struc_bande._constantes import r_0_bar, eta, eps, v_0, z_bar, nc
+from graphene_ge2d.struc_bande._constantes import r_0_bar, eta, eps, v_0, z_bar, nc, e_0_inv
+
+
+class NotImpressedError(NotImplementedError):
+    def __init__(self):
+        super().__init__()
 
 
 def test_matrice_explicite():
@@ -42,7 +48,7 @@ def test_matrice_explicite():
             return v_0 * r_o_bar / (eps * eta)
         bessel_boi = j1(2 * np.pi * norm_g * r_0_bar / eta) / norm_g
         exponentielle = np.exp(- 2 * np.pi * norm_g * z_bar / eta)
-        somme = 1 + np.exp(- 1j * 2 * np.pi * np.dot(g, c2) / eta)
+        somme = 1 #+ np.exp(- 1j * 2 * np.pi * np.dot(g, c2) / eta)
         return v_0 * r_0_bar * bessel_boi * exponentielle * somme / (eps * eta)
 
     diag_principale = np.array([
@@ -63,7 +69,7 @@ def test_matrice_explicite():
             np.dot(g_8, g_8),
             np.dot(g_9, g_9),
     ]) / nc
-    print(f"{diag_principale=}")
+    print(f"{diag_principale*e_0_inv}")
     #print(f"{test_diag=}")
     assert np.isclose(test_diag, diag_principale).all()
     diag_sup = np.array([
@@ -83,9 +89,9 @@ def test_matrice_explicite():
             - coefficients_u(- g_8 + g_9),
     ])
     #print(f"{g_array=}")
-    diags = matrix.get_matrice(7, np.array([0, 0]), 3, g_array)
+    diags = matrix.get_matrice(np.array([0, 0]), 3, g_array)
     #print(f"{diag_principale=}")
-    print(f"{diag_sup=}")
+    print(f"{diag_sup*e_0_inv=}")
     print(f"{diag_inf=}")
     print(f"{diags[:, 0]=}")
     print(f"{diags[:, 1]=}")
@@ -100,3 +106,43 @@ def test_norm_linalg():
     #assert np.isclose(np.linalg.norm(g), )
     assert np.isclose(np.dot(g, g), 4j)
 
+
+def test_code_fortran():
+    data_from_fort = pd.read_table("Data_Exemple/matrice.dat", delim_whitespace=True)
+    diag_princ = []
+    diag_sup = []
+    for i, g in enumerate(data_from_fort['greal']):
+        if data_from_fort['i'][i] == data_from_fort['j'][i]:
+            diag_princ.append(data_from_fort['greal'][i])
+        try:
+            if data_from_fort['i'][i] == data_from_fort['j'][i - 1]:
+                diag_sup.append(data_from_fort['greal'][i])
+        except KeyError:
+            continue
+
+    print(diag_princ)
+    print(diag_sup)
+
+    # Vecteurs de la base
+    b_1 = np.array([0, - 2 / np.sqrt(3)])
+    b_2 = np.array([1, 1 / np.sqrt(3)])
+    c2 = np.array([0, - 1 / np.sqrt(3)])
+
+    # Les G à utiliser dans l'ordre
+    g_1 = np.array([0, 0])
+    g_2 = b_2
+    g_3 = - b_1
+    #g_4 = - b_2 - b_1
+    g_5 = - b_2
+    g_6 = b_1
+    #g_7 = b_2 + b_1
+    g_8 = - b_1 + b_1
+    g_9 = b_2 - b_1
+    g_array = np.array([g_1, g_2, g_3, g_5, g_6, g_8, g_9])
+
+    diags = matrix.get_matrice(np.array([0, 0]), 3, g_array)
+    print(diags[:, 0])
+    print(diags[:, 1])
+    print(diags[:, 0] / diag_princ[:7])
+    print(diags[:, 1] / diag_sup[:7])
+    raise NotImpressedError
