bbeab63a2cbdf49603dbb3c0e4332de0b031a80e
Dimitri <bond2102@usherbrooke.ca>
2022-10-07 11:04:31 -0400

    No loooooose

--- src/graphene_ge2d/struc_bande/eigensolver/src/main.rs
+++ src/graphene_ge2d/struc_bande/eigensolver/src/main.rs
@@ -1,6 +1,9 @@
 extern crate lapack_src;
 extern crate lapack_sys;
 
+use std::fs;
+use std::io::Write;
+use std::path::Path;
 use scilib::math::bessel;
 use rgsl::exponential;
 use num_complex::Complex;
@@ -12,6 +15,7 @@ const ETA: f64 = 1.0;
 const N_COQ: u32 = 15;
 const PI: f64 = std::f64::consts::PI;
 const Z_BAR: f64 = 40.0 / 100.0;
+const E_0: f64 = 22_450.0 / (100.0 * 100.0);
 
 fn coquilles(n_coq: u32) -> (Vec<[f64; 2]>, Vec<[i64; 2]>) {
     let EPS = f64::sqrt(3.0) / 2.0;
@@ -45,56 +49,93 @@ fn potentiel(g: [f64; 2]) -> Complex::<f64> {
 }
 
 fn main() {
-    println!("cargo:rustc-link-lib=/usr/lib/liblapack.a");
+    //println!("cargo:rustc-link-lib=/usr/lib/liblapack.a");
     let EPS = f64::sqrt(3.0) / 2.0;
     let (vecteurs_g, indices_g) = coquilles(N_COQ);
     let n_vect = indices_g.len();
-    let deff_val = Complex::from(V_0 * R_0_BAR / (EPS * ETA));
+    let deff_val = Complex::from(0.0);
 
     // Let's get the potential for all those gs
     let mut pot: Vec<Complex::<f64>> = Vec::new();
     for i in 0..n_vect {
-        pot.push(potentiel(vecteurs_g[i]));
+        //pot.push(potentiel(vecteurs_g[i]));
+        pot.push(Complex::from(0.0));
     }
 
     // Let's gen the potential for all non 0 Gs.
     let mut matrice: Vec<Complex::<f64>> = Vec::new();
     for i in 0..n_vect {
         let g1 = indices_g[i];
-        matrice.push(Complex::from(vecteurs_g[i][0]*vecteurs_g[i][0] + vecteurs_g[i][1]*vecteurs_g[i][1]));
-        for j in (i + 1)..n_vect {
+        for j in 0..i {
             let g2 = indices_g[j];
             for t in 0..n_vect {
-                if (g1[0] - g2[0] == indices_g[t][0]) && (g1[1] - g2[1] == indices_g[t][1]) {
-                    matrice.push(pot[t]);
+                if (g2[0] - g1[0] == indices_g[t][0]) && (g2[1] - g1[1] == indices_g[t][1]) {
+                    matrice.push(- pot[t] / Complex::from(E_0));
                     break;
                 }
                 if t == n_vect - 1 {
+                    //matrice.push(potentiel([vecteurs_g[j][0] - vecteurs_g[i][0], vecteurs_g[j][1] - vecteurs_g[i][1]]));
                     matrice.push(deff_val);
                 }
             }
         }
+        matrice.push(Complex::from(vecteurs_g[i][0]*vecteurs_g[i][0] + vecteurs_g[i][1]*vecteurs_g[i][1]));
     }
     //println!("{:?}", matrice);
     // First let's allocate the working space for LAPACK
-    let mut work: Vec<Complex::<f64>> = Vec::with_capacity(n_vect);
-    let mut rwork: Vec<f64> = Vec::with_capacity(n_vect);
-    let mut iwork: Vec<i32> = Vec::with_capacity(n_vect);
     let ldz: i32 = n_vect as i32;
-    let lwork: i32 = n_vect as i32;
-    let lrwork: i32 = n_vect as i32;
-    let liwork: i32 = n_vect as i32;
+    let lwork: i32 = 2 * n_vect as i32;
+    let lrwork: i32 = 1 + 5 * n_vect as i32 + 2 * (n_vect*n_vect) as i32;
+    let liwork: i32 = 3 + 5 * n_vect as i32;
+    let mut work: Vec<Complex::<f64>> = vec![Complex::from(0.0); lwork.try_into().unwrap()];
+    let mut rwork: Vec<f64> = vec![0.0; lrwork.try_into().unwrap()];
+    let mut iwork: Vec<i32> = vec![0; liwork.try_into().unwrap()];
     let mut info: i32 = 0;
 
     let mut eigen_val: Vec<f64> = vec![0.0; n_vect];
-    println!("{:?}", eigen_val);
     let mut zvec: Vec<Complex::<f64>> = vec![Complex::from(0.0); n_vect];
 
     // Now let's diagonalise it
-    unsafe {
-        zhpevd(b"N"[0], b"U"[0], n_vect as i32, &mut matrice, &mut eigen_val, &mut zvec, ldz,
-                &mut work, lwork, &mut rwork, lrwork, &mut iwork, liwork, &mut info);
+    // We need to take points k on a path to modify the diagonal.
+    let path_chemin = Path::new("raw_data/chemin.txt");
+    let chemin = fs::read_to_string(path_chemin)
+        .expect("Data for path in the Brillouin zone does not exist! Check if it is at the right place.");
+    let mut output_eigval = fs::File::create("raw_data/list_eigvals.txt").expect("Couldn't create output_eigval");
+    for k in chemin.split("\n") {
+        let next_k: Vec<&str> = k.split(" ").collect::<Vec<&str>>();
+        if next_k[0].is_empty() { break; }  // Assume End Of File
+        let next_k: [f64; 2] = [next_k[0].parse().expect("Invalid format in chemin.txt"),
+                                next_k[1].parse().expect("Invalid format in chemin.txt")];
+        diagonalise(&matrice, n_vect as i32, &mut eigen_val, &mut zvec, &ldz,
+                    &mut work, &lwork, &mut rwork, &lrwork, &mut iwork, &liwork, &mut info);
+        let mut strings: Vec<String> = Vec::new();
+        for i in 0..n_vect {
+            let curr_g = vecteurs_g[i];
+            // The diagonal elements are always i away from the last one
+            matrice[i*(i + 1) / 2] = Complex::from((curr_g[0] + next_k[0]).powf(2.0) + (curr_g[1] + next_k[1]).powf(2.0));
+            strings.push(eigen_val[i].to_string());
+        }
+        writeln!(output_eigval, "{}", strings.join(" "));
     }
-    println!("{}", info);
-    println!("{:?}", eigen_val);
+}
+
+fn diagonalise(
+    matrice: &[Complex::<f64>],  // Reference to the matrix you want to diagonalise
+    n_vect: i32,                 // Dimension of the matrix (n_vect*n_vect)
+    eigen_val: &mut [f64],       // A mutable reference to the output vector of eigenvalues
+    zvec: &mut [Complex::<f64>], // A mutable reference to the output matrix of eigenvectors
+    ldz: &i32,                   // The dimension of zvec
+    work: &mut [Complex::<f64>], // zhpevd working memory for all below
+    lwork: &i32,
+    rwork: &mut [f64],
+    lrwork: &i32,
+    iwork: &mut [i32],
+    liwork: &i32,
+    info: &mut i32               // An int representing if it converged or not
+) {  // Function changes by-reference the vector eigen_val
+    let mut vec_mat = matrice.to_vec();
+    unsafe {
+        zhpevd(b"N"[0], b"U"[0], n_vect as i32, &mut vec_mat, eigen_val, zvec, *ldz,
+                work, *lwork, rwork, *lrwork, iwork, *liwork, info);
+    }
 }
